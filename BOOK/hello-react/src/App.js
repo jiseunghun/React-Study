e6a25b96bbf66b9dfe0308059c0d322edf23d2d2
Sunhye Moon <ss333567@gmail.com>
2022-01-17 09:55:24 +0900

    220117- study

---
+++ BOOK/hello-react/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  const name = "React";
+  return (
+    <>
+      {/* 주석은 이렇게 작성합니다. */}
+      <div
+        className="react" // 시작태그를 여러줄 작성
+      >
+        {name}
+      </div>
+      <input />
+    </>
+  );
+}
+
+export default App;
